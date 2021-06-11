3b32a473c4ff81867c98a1d40d21da70eee1ce6d
vpravdi <dineshpraveen91@gmail.com>
2021-06-11 23:39:25 +0530

    arrays v3

---
+++ Arrays/Two number sum/0(n log n)/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "sort"
+
+func TwoNumberSum(array []int, target int) []int {
+
+	sort.Ints(array)
+	left, right := 0, len(array)-1
+	for left < right {
+		sum := array[left] + array[right]
+		if sum == target {
+			return []int{array[left], array[right]}
+		} else if sum < target {
+			left += 1
+		} else {
+			right -= 1
+
+		}
+	}
+
+	return []int{}
+}
